b5cc374bcced1d3182983caf49baffa862572b3e
Denny Feng <fengdennyy@gmail.com>
2024-03-28 16:48:18 -0700

    Refactored functions and created retrieveNewestCollections / extractPageHeader function

--- tvbScrapper/scrapper.js
+++ tvbScrapper/scrapper.js
@@ -1,8 +1,7 @@
 import puppeteer from "puppeteer";
-import fs from "fs";
-
+import { readDataFromFile, checkFileData } from './dataCreation.js';
 async function setupPage(url) {
-  const browser = await puppeteer.launch();
+  const browser = await puppeteer.launch({headless:false, defaultViewport:null});
   const page = await browser.newPage();
   await page.goto(url);
   return { browser, page };
@@ -12,65 +11,88 @@ async function closeBrowser(browser) {
   return browser.close();
 }
 
-async function retrieveData(url) {
-  try {
-    const { browser, page } = await setupPage(url);
-    const data = await page.evaluate(() => {
+async function extractPageHeader(page){
+ const headerData = await page.evaluate(() => {
+    let header = document.querySelector(".collection-header")?.textContent.trim() 
+    return header
+ })
+ return headerData
+}
+
+async function extractDramasData(page, ) {
+  const data = await page.evaluate(() => {
       const dramasMap = new Map(); // Use a Map to store image source URLs for each title
       document.querySelectorAll(".drama").forEach((drama) => {
-        const link = drama.querySelector("a");
-        if (link) {
-          const titleElement = link.querySelector(".title");
-          const imgElement = link.querySelector("img");
-          if (titleElement && imgElement) {
-            const title = titleElement.textContent.trim();
-            const imgSrc = imgElement.getAttribute("src");
-            if (!dramasMap.has(title)) {
-              dramasMap.set(title, []); // Use title as key
-            }
-            const imgSrcArray = dramasMap.get(title);
-            if (!imgSrcArray.some((item) => item.imgSrc === imgSrc)) {
-              imgSrcArray.push({ imgSrc }); // Push imgSrc to array associated with title if not already present
-            }
+          const link = drama.querySelector("a");
+          if (link) {
+              const titleElement = link.querySelector(".title");
+              const imgElement = link.querySelector("img");
+              if (titleElement && imgElement) {
+                  const title = titleElement.textContent.trim();
+                  const imgSrc = imgElement.getAttribute("src");
+                  if (!dramasMap.has(title)) {
+                      dramasMap.set(title, []); // Use title as key
+                  }
+                  const imgSrcArray = dramasMap.get(title);
+                  if (!imgSrcArray.some((item) => item.imgSrc === imgSrc)) {
+                      imgSrcArray.push({ imgSrc }); // Push imgSrc to array associated with title if not already present
+                  }
+              } else {
+                  console.error("Title or image element not found in drama:", drama);
+              }
           } else {
-            console.error("Title or image element not found in drama:", drama);
+              console.error("Link element not found in drama:", drama);
           }
-        } else {
-          console.error("Link element not found in drama:", drama);
-        }
       });
-      return Object.fromEntries(dramasMap); // Convert Map to a regular object
-    });
+      return Object.fromEntries(dramasMap) ;
+  });
+  return data;
+}
+
+async function retrieveAllDataWithHeader(url, dataFile){
+  try {
+    const { browser, page } = await setupPage(url);
+    const dramasData = await extractDramasData(page)
+    const headerData = await extractPageHeader(page)
+    const dramasArray = new Array()
+    dramasArray.push(dramasData)
+    const data = { header: headerData, dramas: dramasArray };
     await closeBrowser(browser);
-
-    const dataStringified = JSON.stringify(data, null, 2);
-    // Read existing data from file
-    let existingData = {};
-    if (fs.existsSync("data.json")) {
-      const existingDataString = fs.readFileSync("data.json", "utf8");
-      existingData = JSON.parse(existingDataString);
-    }
-
-    // Compare existing data with new data
-    const newDataString = dataStringified;
-    const existingDataString = JSON.stringify(existingData, null, 2);
-    if (newDataString !== existingDataString) {
-      // Write data to JSON file only if it's different
-      fs.writeFileSync("data.json", newDataString);
-      console.log("Data saved to data.json");
-    } else {
-      console.log("Data in file is up to date, no changes made");
-    }
-
-    return dataStringified;
+    const existingData = readDataFromFile(dataFile)
+    const newDataString = JSON.stringify(existingData, null, 2)
+    checkFileData(dataFile, existingData, newDataString, data)
+    return newDataString
   } catch (error) {
     console.error("Error retrieving data:", error);
     return {};
   }
 }
 
-const tvbUrl = "https://tvbanywherena.com/english";
 
-retrieveData(tvbUrl)
-  .then((result) => console.log(`Data: ${result}`))
+async function retrieveAllShowsData(url) {
+  const dataFile = "allShowsData.json";
+  return await retrieveAllDataWithHeader(url, dataFile)
+}
+
+async function retrieveNewestCollections(url){
+   const dataFile = "2024DramaCollections.json"
+   return await retrieveAllDataWithHeader(url, dataFile)
+}
+
+const tvbUrlEnglish = "https://tvbanywherena.com/english";
+const tvbURL2024Collections ="https://tvbanywherena.com/english/collections/USA_2024"
+
+retrieveAllShowsData(tvbUrlEnglish)
+  .then((result) => {
+    const data = JSON.parse(result);
+    console.log(`Dramas: ${JSON.stringify(data.dramas, null, 2)}`)
+  })
+  .catch((error) => console.error(`Error: ${error}`));
+
+retrieveNewestCollections(tvbURL2024Collections)
+  .then((result) => {
+    const data = JSON.parse(result);
+    console.log(`Newest Collections - ${data.header}`); 
+    console.log(`Dramas: ${JSON.stringify(data.dramas, null, 2)}`)
+  })
   .catch((error) => console.error(`Error: ${error}`));
