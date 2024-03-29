c9b1260ffe0729ce030033141f845d34f093274e
Denny Feng <fengdennyy@gmail.com>
2024-03-29 00:52:37 -0700

    Refactored and added links and banner image data to hash map to import to json file

--- tvbScrapper/scrapper.js
+++ tvbScrapper/scrapper.js
@@ -3,6 +3,7 @@ import { readDataFromFile, checkFileData } from './dataCreation.js';
 async function setupPage(url) {
   const browser = await puppeteer.launch({headless:false, defaultViewport:null});
   const page = await browser.newPage();
+  page.setDefaultNavigationTimeout(60000);  // allow more time for the page to load for big data
   await page.goto(url);
   return { browser, page };
 }
@@ -12,50 +13,89 @@ async function closeBrowser(browser) {
 }
 
 async function extractPageHeader(page){
- const headerData = await page.evaluate(() => {
-    let header = document.querySelector(".collection-header")?.textContent.trim() 
-    return header
- })
- return headerData
+  const headerData = await page.evaluate( () => {
+     let header = document.querySelector(".collection-header")?.textContent.trim() 
+     return header
+  })
+  return headerData
+ }
+ 
+
+async function processData(page, drama, dramasMap) {
+  const { title, imgSrc, hrefLink } = drama;
+
+  if (!dramasMap.has(title)) {
+    dramasMap.set(title, []);
+  }
+
+  const lookupDrama = dramasMap.get(title);
+  if (!lookupDrama.some((item) => item.imgSrc === imgSrc)) {
+    lookupDrama.push({ imgSrc, link: hrefLink });
+  }
+
+  for (const dramaItem of lookupDrama) {
+    const dramaHref = dramaItem.link;
+    try {
+      await page.goto(dramaHref);
+      const bannerImgSrc = await page.evaluate(() => {
+        const bannerDiv = document.querySelector(".banner-div");
+        if (bannerDiv) {
+          const imgElement = bannerDiv.querySelector("img");
+          if (imgElement) return imgElement.getAttribute("src");
+        }
+        return null;
+      });
+
+      lookupDrama.forEach((drama) => drama.banner = bannerImgSrc);
+
+    } catch (error) {
+      console.error("Error fetching document:", error);
+    }
+  }
 }
 
-async function extractDramasData(page, ) {
-  const data = await page.evaluate(() => {
-      const dramasMap = new Map(); // Use a Map to store image source URLs for each title
-      document.querySelectorAll(".drama").forEach((drama) => {
-          const link = drama.querySelector("a");
-          if (link) {
-              const titleElement = link.querySelector(".title");
-              const imgElement = link.querySelector("img");
-              if (titleElement && imgElement) {
-                  const title = titleElement.textContent.trim();
-                  const imgSrc = imgElement.getAttribute("src");
-                  if (!dramasMap.has(title)) {
-                      dramasMap.set(title, []); // Use title as key
-                  }
-                  const imgSrcArray = dramasMap.get(title);
-                  if (!imgSrcArray.some((item) => item.imgSrc === imgSrc)) {
-                      imgSrcArray.push({ imgSrc }); // Push imgSrc to array associated with title if not already present
-                  }
-              } else {
-                  console.error("Title or image element not found in drama:", drama);
-              }
-          } else {
-              console.error("Link element not found in drama:", drama);
-          }
-      });
-      return Object.fromEntries(dramasMap) ;
+
+async function extractDramasData(page) {
+  const dramasMap = new Map();
+
+  const dramaElements = await page.evaluate(() => {
+    const dramas = Array.from(document.querySelectorAll('.drama'));
+    return dramas.map(drama => {
+      const link = drama.querySelector('a');
+      if (!link) {
+        console.error("Link element not found in drama:", drama);
+        return null;
+      }
+      
+      const titleElement = link.querySelector('.title');
+      const imgElement = link.querySelector('img');
+
+      if (titleElement && imgElement) {
+        const title = titleElement.textContent.trim();
+        const imgSrc = imgElement.getAttribute('src');
+        const hrefLink = link.href;
+        return { title, imgSrc,  hrefLink };
+      } else {
+        console.error("Title or image element not found in drama:", drama);
+        return null;
+      }
+    }).filter(Boolean); // Ensures that only valid data (non-null) is included in the resulting array.
   });
-  return data;
+
+  for (const drama of dramaElements) {
+    if (!drama) continue;
+    await processData(page, drama, dramasMap);
+  }
+
+  return Object.fromEntries(dramasMap);
 }
 
 async function retrieveAllDataWithHeader(url, dataFile){
   try {
     const { browser, page } = await setupPage(url);
-    const dramasData = await extractDramasData(page)
     const headerData = await extractPageHeader(page)
-    const dramasArray = new Array()
-    dramasArray.push(dramasData)
+    const dramasData = await extractDramasData(page)
+    const dramasArray = [dramasData]
     const data = { header: headerData, dramas: dramasArray };
     await closeBrowser(browser);
     const existingData = readDataFromFile(dataFile)
